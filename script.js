56148517e0d0205bbea03e4ad8ae839dcc6df2f2
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-11 20:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans="#";
+	let i;
+	if(shortHex.length==4) i=1;
+	else i=0;
+	for(i;i<shortHex.length;i++){
+		ans+=shortHex[i]
+		ans+=shortHex[i];
+	}
+	return ans;
 };
 
 // Do not change the code below.
