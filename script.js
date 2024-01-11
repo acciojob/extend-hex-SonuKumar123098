fc18774676cf9eb69b5c290e7ccc7027d9919080
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-11 21:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let ans="#";
-	let i;
-	if(shortHex.length==4) i=1;
-	else i=0;
+	let i=shortHex.length-3;
+	let isCapital=false;
 	for(i;i<shortHex.length;i++){
 		ans+=shortHex[i]
 		ans+=shortHex[i];
+		let asciiCode=shortHex.charCodeAt(i);
+		if(asciiCode>=65 && asciiCode<=90) isCapital=true;
 	}
+	if(isCapital)return ans.toUpperCase();
 	return ans;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
